d3ece8bb1493b958aa912b9db55ee993c9da5858
Dozie <offor919@gmail.com>
2026-05-02 01:55:03 +0100

    feat: register User, SchoolClass, and Department entities in StudentModule

--- src/student/student.module.ts
+++ src/student/student.module.ts
@@ -5,9 +5,12 @@ import { StudentService } from './student.service';
 import { Student } from '../profile/entities/models/student.entity';
 import { Assignment } from '../assignments/entities/assignment.entity';
 import { WeeklyReport } from '../reports/entities/weekly-report.entity';
+import { User } from '../users/entities/user.entity';
+import { SchoolClass } from '../academics/entities/school-class.entity';
+import { Department } from '../academics/entities/department.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Student, Assignment, WeeklyReport])],
+  imports: [TypeOrmModule.forFeature([Student, Assignment, WeeklyReport, User, SchoolClass, Department])],
   controllers: [StudentController],
   providers: [StudentService],
 })
