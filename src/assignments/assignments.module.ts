6e550c9b8ccbadb9cb60fc38a8d629fd0c3c6b58
Dozie <offor919@gmail.com>
2026-05-02 01:12:42 +0100

    feat: register Teacher and SchoolClass entities in AssignmentsModule

--- src/assignments/assignments.module.ts
+++ src/assignments/assignments.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Assignment } from './entities/assignment.entity';
+import { Teacher } from '../profile/entities/models/teacher.entity';
+import { SchoolClass } from '../academics/entities/school-class.entity';
 import { AssignmentsService } from './assignments.service';
 import { AssignmentsController } from './assignments.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Assignment])],
+  imports: [TypeOrmModule.forFeature([Assignment, Teacher, SchoolClass])],
   controllers: [AssignmentsController],
   providers: [AssignmentsService],
 })
