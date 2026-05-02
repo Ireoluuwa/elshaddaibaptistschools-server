53c303644e6e7783be7e3bd355d7e4762f2bed3a
Dozie <offor919@gmail.com>
2026-05-02 12:47:47 +0100

    fix: update CORS origin to use environment variable for local frontend URL

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.useGlobalInterceptors(new TransformInterceptor(new Reflector()));
   app.enableCors({
-    origin: ['http://localhost:3000', process.env.FRONTEND_URL].filter(Boolean),
+    origin: [process.env.LOCAL_FRONTEND_URL, process.env.FRONTEND_URL].filter(Boolean),
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
