795104cb1dd5874602cfab49477b1d595fb649ad
Dozie <offor919@gmail.com>
2026-05-04 06:49:06 +0100

    feat: register Subject and Curriculum entities in AcademicsModule

--- src/academics/academics.module.ts
+++ src/academics/academics.module.ts
@@ -4,12 +4,21 @@ import { SchoolClass } from './entities/school-class.entity';
 import { Department } from './entities/department.entity';
 import { AcademicYear } from './entities/academic-year.entity';
 import { Term } from './entities/term.entity';
+import { Subject } from './entities/subject.entity';
+import { Curriculum } from './entities/curriculum.entity';
 import { AcademicsService } from './academics.service';
 import { AcademicsController } from './academics.controller';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([SchoolClass, Department, AcademicYear, Term]),
+    TypeOrmModule.forFeature([
+      SchoolClass,
+      Department,
+      AcademicYear,
+      Term,
+      Subject,
+      Curriculum,
+    ]),
   ],
   controllers: [AcademicsController],
   providers: [AcademicsService],
